cf5a5c5b0edf2561e60304031b9ea28aeb0d16c6
Sara Peel <sarampeel@gmail.com>
2024-02-20 10:18:19 -0700

    Jsx

--- src/FoodCard.jsx
+++ src/FoodCard.jsx
@@ -2,6 +2,13 @@
 import React from 'react';
 import './FoodCard.css';
 
+// class code
+// class title
+// description
+// credit hours
+// when taught
+// prerequisites
+
 const FoodCard = ({ name, description }) => {
   return (
     <div className="food-card">
