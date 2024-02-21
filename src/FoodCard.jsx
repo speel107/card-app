49a7539e0744956ea08d8336cd4138157cd0988a
ethanmchls <ethanmchls@gmail.com>
2024-02-21 09:50:35 -0700

    added close button to modal menu

--- src/FoodCard.jsx
+++ src/FoodCard.jsx
@@ -18,9 +18,15 @@ const FoodCard = ({ name, description }) => {
         <div className="modal-box">
           <h3 className="font-bold text-lg">Hello!</h3>
           <p className="py-4">Press ESC key or click outside to close</p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
         </div>
         <form method="dialog" className="modal-backdrop">
-          <button>close</button>
+          <button></button>
         </form>
       </dialog>
     </div>
