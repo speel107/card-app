4523ab03ef6b3ee473f152d9ee690bc8484f9329
Rebekah Ross <rebekahross123@gmail.com>
2024-02-21 10:27:32 -0700

    added ".0 credits"

--- src/ClassCard.jsx
+++ src/ClassCard.jsx
@@ -1,6 +1,6 @@
 // ClassCard.jsx
-import React from 'react';
-import './ClassCard.css';
+import React from "react";
+import "./ClassCard.css";
 
 // class code*
 // class title*
@@ -11,24 +11,31 @@ import './ClassCard.css';
 
 const ClassCard = ({ course }) => {
   return (
-    <div onClick={()=>document.getElementById(`${course.name}-modal`).showModal()} className="class-card hover:shadow-lg hover:scale-105">
-      <h3>{course.code} - {course.credits}</h3>
+    <div
+      onClick={() =>
+        document.getElementById(`${course.name}-modal`).showModal()
+      }
+      className="class-card hover:shadow-lg hover:scale-105"
+    >
+      <h3>
+        {course.code} - {course.credits}.0 credits
+      </h3>
       <p>{course.name}</p>
-    <dialog id={`${course.name}-modal`} className="modal">
-      <div className="modal-box">
-        <h3 className="font-bold text-lg">{course.name}</h3>
-        <p className="py-4">{course.description}</p>
-        <div className="modal-action">
-          <form method="dialog">
-            <button className="btn">Close</button>
-          </form>
+      <dialog id={`${course.name}-modal`} className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">{course.name}</h3>
+          <p className="py-4">{course.description}</p>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
         </div>
-      </div>
-      <form method="dialog" className="modal-backdrop">
-        <button></button>
-      </form>
-    </dialog>
-  </div>
+        <form method="dialog" className="modal-backdrop">
+          <button></button>
+        </form>
+      </dialog>
+    </div>
   );
 };
 
