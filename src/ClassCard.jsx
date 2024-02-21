3989d001f403cf9cebacd7c29669dfcb94e22bd6
Sara Peel <sarampeel@gmail.com>
2024-02-21 10:16:08 -0700

    Card clickable w/ modal now

--- src/ClassCard.jsx
+++ src/ClassCard.jsx
@@ -11,10 +11,24 @@ import './ClassCard.css';
 
 const ClassCard = ({ code, credits, name }) => {
   return (
-    <div className="class-card">
+    <div onClick={()=>document.getElementById(`${name}-modal`).showModal()} className="class-card hover:shadow-lg hover:scale-105">
       <h3>{code} - {credits}</h3>
       <p>{name}</p>
-    </div>
+    <dialog id={`${name}-modal`} className="modal">
+      <div className="modal-box">
+        <h3 className="font-bold text-lg">Hello!</h3>
+        <p className="py-4">Press ESC key or click outside to close</p>
+        <div className="modal-action">
+          <form method="dialog">
+            <button className="btn">Close</button>
+          </form>
+        </div>
+      </div>
+      <form method="dialog" className="modal-backdrop">
+        <button></button>
+      </form>
+    </dialog>
+  </div>
   );
 };
 
