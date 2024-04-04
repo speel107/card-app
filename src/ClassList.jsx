55febc39e08b1e828bc76a27455065eb6edd2ae3
ethanmchls <ethanmchls@gmail.com>
2024-04-03 20:17:39 -0600

    added all button and fixed some ui things according to the feedback

--- src/ClassList.jsx
+++ src/ClassList.jsx
@@ -4,7 +4,7 @@ import classData from "./classes.json";
 import ClassCard from "./ClassCard";
 import "./ClassList.css";
 
-const categories = ["100", "200", "300", "400"];
+const categories = ["100", "200", "300", "400", "All"];
 const subcategories = {
   100: ["Intro", "Math"],
   200: [
@@ -29,21 +29,27 @@ const subcategories = {
     "Bioinformatics",
     "Machine Learning",
   ],
+  All: [],
 };
 
 const ClassList = () => {
-  const [category, setCategory] = useState("100");
+  const [category, setCategory] = useState("All");
   const [subcategory, setSubcategory] = useState(null);
   const [subCategoriesLen, setSubCategoriesLen] = useState("w-1/" + (subcategories["100"].length).toString());
 
   console.log(subCategoriesLen);
 
-  const filteredClasses = subcategory
+  var filteredClasses = subcategory
     ? classData.filter(
         (classCS) =>
           classCS.category === category && classCS.subcategory === subcategory
       )
     : classData.filter((classCS) => classCS.category === category);
+  
+  if (category === "All") {
+    filteredClasses = classData;
+    // setSubcategory(null);
+  }
 
   return (
     <div className="flex flex-col my-4 mx-auto w-3/4 pt-2 mt-0 px-2 mb-auto overflow-y-scroll">
@@ -52,13 +58,13 @@ const ClassList = () => {
           <div className="divider divider-neutral mt-0 font-bold text-primary">Course Level</div>
           <div className="tabs flex justify-between -mr-2">
             {categories.map((cat) => (
-              <div className="w-1/4 mr-2">
+              <div className="w-1/5 mr-2">
                 <button
                   key={cat}
                   onClick={() => {
                     setCategory(cat);
                     setSubcategory(null);
-                    setSubCategoriesLen("w-1/" + (subcategories["100"].length).toString());
+                    setSubCategoriesLen("w-1/" + (subcategories[cat].length).toString());
                   }}
                   className={`${
                     category === cat ? "selected" : ""
@@ -69,8 +75,9 @@ const ClassList = () => {
               </div>
             ))}
           </div>
-          <div className="divider divider-neutral mt-2 font-bold text-primary">Category</div>
+          <div className={`divider divider-neutral mt-2 font-bold text-primary`}>{category === "All" ? "All CS Courses" : "Subcategory"}</div>
           {category && (
+            category !== "All" &&
             <div className="tabs flex justify-between -mr-2">
               {subcategories[category].map((subcat) => (
                 <div className={`${subCategoriesLen} mr-2`}>
@@ -90,9 +97,8 @@ const ClassList = () => {
               ))}
             </div>
           )}
-          <div className="divider divider-neutral mt-2 mb-0"></div>
+          <div className={`divider divider-neutral mt-2 mb-0 ${category === "All" ? "hidden" : ""}`}></div>
         </div>
-      {/* </div> */}
       <div className="flex my-0 mx-auto w-full p-2 overflow-y-scroll">
         <div className="grid grid-flow-row text-neutral-600 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-between gap-4">
           {filteredClasses.map((classCS) => (
@@ -100,6 +106,7 @@ const ClassList = () => {
           ))}
         </div>
       </div>
+      <div className="h-4"></div>
     </div>
   );
 };
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="header bg-primary text-neutral-content">
+    <header className="header bg-primary text-base-100">
       <div className="container mx-auto pt-3 pb-4">
         <div className="flex-1">
           <h1 className="text-4xl font-bold px-8">BYU CS Classes</h1>
